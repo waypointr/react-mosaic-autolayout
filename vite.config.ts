0b2b7feb0a5d22578d26f0bcdc5e7fdd251a100b
Ian Garrison <img4@cornell.edu>
2023-03-02 12:38:11 -0600

    cleaning up the build to properly exclude react dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import packageJson from './package.json' assert { type: 'json' };
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig((_configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    dts({
+      include: ['src'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      name: 'react-mosaic-autolayout',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-mosaic-autolayout.${format}.js`,
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys({
+          ...packageJson.peerDependencies,
+          'react/jsx-runtime': null,
+          'react-dom/server': null
+        }),
+      ],
+    },
+  },
+}));
